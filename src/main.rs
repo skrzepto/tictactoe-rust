e8c63a72911dcf62732768fa6e7d07e50953c2b8
Simon Mucha <shims506@gmail.com>
2018-04-24 19:59:38 -0500

    created board and compiling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod board {
+    #[derive(Copy, Clone)]
+    pub enum Square {
+        Empty,
+        Black,
+        White
+    }
+
+    pub fn init () -> [[Square;3]; 3] {
+        [[Square::Empty; 3]; 3]
+    }
+}
+
 fn main() {
+    let board = board::init();
     println!("Hello, world!");
 }
